1f11d4d93e999f42f7a4563c8415d85b4d33a739
Matt Davis <jiffyclub@gmail.com>
2015-04-30 14:39:54 -0700

    model -> step for orca compatibility

--- models.py
+++ models.py
@@ -7,74 +7,74 @@ import utils
 import variables
 
 
-@orca.model('rsh_estimate')
+@orca.step('rsh_estimate')
 def rsh_estimate(buildings, zones):
     return utils.hedonic_estimate("rsh.yaml", buildings, zones)
 
 
-@orca.model('rsh_simulate')
+@orca.step('rsh_simulate')
 def rsh_simulate(buildings, zones):
     return utils.hedonic_simulate("rsh.yaml", buildings, zones,
                                   "residential_sales_price")
 
 
-@orca.model('nrh_estimate')
+@orca.step('nrh_estimate')
 def nrh_estimate(buildings, zones):
     return utils.hedonic_estimate("nrh.yaml", buildings, zones)
 
 
-@orca.model('nrh_simulate')
+@orca.step('nrh_simulate')
 def nrh_simulate(buildings, zones):
     return utils.hedonic_simulate("nrh.yaml", buildings, zones,
                                   "non_residential_rent")
 
 
-@orca.model('hlcm_estimate')
+@orca.step('hlcm_estimate')
 def hlcm_estimate(households, buildings, zones):
     return utils.lcm_estimate("hlcm.yaml", households, "building_id",
                               buildings, zones)
 
 
-@orca.model('hlcm_simulate')
+@orca.step('hlcm_simulate')
 def hlcm_simulate(households, buildings, zones):
     return utils.lcm_simulate("hlcm.yaml", households, buildings, zones,
                               "building_id", "residential_units",
                               "vacant_residential_units")
 
 
-@orca.model('elcm_estimate')
+@orca.step('elcm_estimate')
 def elcm_estimate(jobs, buildings, zones):
     return utils.lcm_estimate("elcm.yaml", jobs, "building_id",
                               buildings, zones)
 
 
-@orca.model('elcm_simulate')
+@orca.step('elcm_simulate')
 def elcm_simulate(jobs, buildings, zones):
     return utils.lcm_simulate("elcm.yaml", jobs, buildings, zones,
                               "building_id", "job_spaces", "vacant_job_spaces")
 
 
-@orca.model('households_relocation')
+@orca.step('households_relocation')
 def households_relocation(households):
     return utils.simple_relocation(households, .05, "building_id")
 
 
-@orca.model('jobs_relocation')
+@orca.step('jobs_relocation')
 def jobs_relocation(jobs):
     return utils.simple_relocation(jobs, .05, "building_id")
 
 
-@orca.model('households_transition')
+@orca.step('households_transition')
 def households_transition(households):
     return utils.simple_transition(households, .05, "building_id")
 
 
-@orca.model('jobs_transition')
+@orca.step('jobs_transition')
 def jobs_transition(jobs):
     return utils.simple_transition(jobs, .05, "building_id")
 
 
-@orca.model('feasibility')
+@orca.step('feasibility')
 def feasibility(parcels):
     utils.run_feasibility(parcels,
                           variables.parcel_average_price,
@@ -93,7 +93,7 @@ def add_extra_columns(df):
     return df
 
 
-@orca.model('residential_developer')
+@orca.step('residential_developer')
 def residential_developer(feasibility, households, buildings, parcels, year):
     utils.run_developer("residential",
                         households,
@@ -110,7 +110,7 @@ def residential_developer(feasibility, households, buildings, parcels, year):
                         bldg_sqft_per_job=400.0)
 
 
-@orca.model('non_residential_developer')
+@orca.step('non_residential_developer')
 def non_residential_developer(feasibility, jobs, buildings, parcels, year):
     utils.run_developer(["office", "retail", "industrial"],
                         jobs,
